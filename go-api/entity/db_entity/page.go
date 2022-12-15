d280dcc6649f9373cf087432d0e50955eae0cfd8
i16fujimoto <fujimoto.ryusei@arakawa-lab.com>
2022-12-16 06:28:33 +0900

    DBのカラムを変更

--- go-api/entity/db_entity/page.go
+++ go-api/entity/db_entity/page.go
@@ -11,10 +11,11 @@ import (
 
 // QuestionのSubCollection
 type Answer struct {
-	Id  uint `json:"id" bson:"_id"`
+	Id primitive.ObjectID `json:"id" bson:"_id"`
 	Detail string `json:"detail" bson:"detail"`
+	Image string `json:"image" bson:"image"`
 	Respondent User `json:"respondent" bson:"respondent"`
-	Like []User `json:"like" bson:"like"`
+	Like []primitive.ObjectID `json:"like" bson:"like"` // User.ObjectID
 	CreatedAt  time.Time  `json:"createdAt" bson:"createdAt"`
 	UpdatedAt  time.Time  `json:"updatedAt" bson:"updatedAt"`
 	DeletedAt  *time.Time `json:"deletedAt" bson:"deletedAt"`
@@ -29,33 +30,6 @@ func (a *Answer) MarshalBSON() ([]byte, error) {
     
     type my Answer
     return bson.Marshal((*my)(a))
-}
-
-type Question struct {
-	Id primitive.ObjectID `json:"id" bson:"_id"`
-	Detail string `json:"detail" bson:"detail"`
-	Image []string `json:"image" bson:"image"` // 質問内に挿入する画像のパス
-	Questioner User `json:"questioner" bson:"questioner"`
-	Like []User `json:"like" bson:"like"`
-	Priority string `json:"priority" bson:"priority"` // 緊急 or なるはや or まったり 等
-	Status string `json:"status" bson:"status"` // 回答募集中 or 沼り中 or 解決済 等
-	Category []string `json:"category" bson:"category"` // カテゴリーテーブルのカテゴリー名を挿入
-	Language string `json:"language" bson:"language"` // 言語テーブル内の言語名を挿入
-	Answer []Answer `json:"answer" bson:"answer"` // SubCollection
-	CreatedAt  time.Time  `json:"createdAt" bson:"createdAt"`
-	UpdatedAt  time.Time  `json:"updatedAt" bson:"updatedAt"`
-	DeletedAt  *time.Time `json:"deletedAt" bson:"deletedAt"`
-}
-
-// Auto Create CreatedAt, UpdatedAt
-func (q *Question) MarshalBSON() ([]byte, error) {
-    if q.CreatedAt.IsZero() {
-        q.CreatedAt = time.Now()
-    }
-    q.UpdatedAt = time.Now()
-    
-    type my Question
-    return bson.Marshal((*my)(q))
 }
 
 type Category struct {
@@ -78,46 +52,74 @@ func (c *Category) MarshalBSON() ([]byte, error) {
     return bson.Marshal((*my)(c))
 }
 
-type Language struct {
+type Question struct {
 	Id primitive.ObjectID `json:"id" bson:"_id"`
-	LanguageName string `json:"languageName" bson:"languageName"`
+	Title string `json:"title" bson:"title"`
+	Detail string `json:"detail" bson:"detail"`
+	Image []string `json:"image" bson:"image"` // 質問内に挿入する画像のパス
+	Questioner User `json:"questioner" bson:"questioner"`
+	Like []primitive.ObjectID `json:"like" bson:"like"` // USer.ObjectID
+	Priority string `json:"priority" bson:"priority"` // 緊急 or なるはや or まったり 等
+	Status string `json:"status" bson:"status"` // 回答募集中 or 沼り中 or 解決済 等
+	Category []primitive.ObjectID `json:"category" bson:"category"` // カテゴリーテーブルのカテゴリー名を挿入 Category.ObjectID
+	// Language string `json:"language" bson:"language"` // 言語テーブル内の言語名を挿入
+	Answer []Answer `json:"answer" bson:"answer"` // SubCollection
 	CreatedAt  time.Time  `json:"createdAt" bson:"createdAt"`
 	UpdatedAt  time.Time  `json:"updatedAt" bson:"updatedAt"`
 	DeletedAt  *time.Time `json:"deletedAt" bson:"deletedAt"`
 }
 
 // Auto Create CreatedAt, UpdatedAt
-func (l *Language) MarshalBSON() ([]byte, error) {
-    if l.CreatedAt.IsZero() {
-        l.CreatedAt = time.Now()
+func (q *Question) MarshalBSON() ([]byte, error) {
+    if q.CreatedAt.IsZero() {
+        q.CreatedAt = time.Now()
     }
-    l.UpdatedAt = time.Now()
+    q.UpdatedAt = time.Now()
     
-    type my Language
-    return bson.Marshal((*my)(l))
+    type my Question
+    return bson.Marshal((*my)(q))
 }
+
+// type Language struct {
+// 	Id primitive.ObjectID `json:"id" bson:"_id"`
+// 	LanguageName string `json:"languageName" bson:"languageName"`
+// 	CreatedAt  time.Time  `json:"createdAt" bson:"createdAt"`
+// 	UpdatedAt  time.Time  `json:"updatedAt" bson:"updatedAt"`
+// 	DeletedAt  *time.Time `json:"deletedAt" bson:"deletedAt"`
+// }
+
+// // Auto Create CreatedAt, UpdatedAt
+// func (l *Language) MarshalBSON() ([]byte, error) {
+//     if l.CreatedAt.IsZero() {
+//         l.CreatedAt = time.Now()
+//     }
+//     l.UpdatedAt = time.Now()
+    
+//     type my Language
+//     return bson.Marshal((*my)(l))
+// }
 
 // tipsページ
 
-type Tips struct {
-	Id primitive.ObjectID `json:"id" bson:"_id"`
-	Details string `json:"details" bson:"details"`
-	Image string `json:"image" bson:"image"`
-	Category []string `json:"category" bson:"category"`
-	Author User `json:"author" bson:"author"`
-	Like []User `json:"like" bson:"like"`
-	CreatedAt  time.Time  `json:"createdAt" bson:"createdAt"`
-	UpdatedAt  time.Time  `json:"updatedAt" bson:"updatedAt"`
-	DeletedAt  *time.Time `json:"deletedAt" bson:"deletedAt"`
-}
+// type Tips struct {
+// 	Id primitive.ObjectID `json:"id" bson:"_id"`
+// 	Details string `json:"details" bson:"details"`
+// 	Image string `json:"image" bson:"image"`
+// 	Category []string `json:"category" bson:"category"`
+// 	Author User `json:"author" bson:"author"`
+// 	Like []User `json:"like" bson:"like"`
+// 	CreatedAt  time.Time  `json:"createdAt" bson:"createdAt"`
+// 	UpdatedAt  time.Time  `json:"updatedAt" bson:"updatedAt"`
+// 	DeletedAt  *time.Time `json:"deletedAt" bson:"deletedAt"`
+// }
 
-// Auto Create CreatedAt, UpdatedAt
-func (t *Tips) MarshalBSON() ([]byte, error) {
-    if t.CreatedAt.IsZero() {
-        t.CreatedAt = time.Now()
-    }
-    t.UpdatedAt = time.Now()
+// // Auto Create CreatedAt, UpdatedAt
+// func (t *Tips) MarshalBSON() ([]byte, error) {
+//     if t.CreatedAt.IsZero() {
+//         t.CreatedAt = time.Now()
+//     }
+//     t.UpdatedAt = time.Now()
     
-    type my Tips
-    return bson.Marshal((*my)(t))
-}
+//     type my Tips
+//     return bson.Marshal((*my)(t))
+// }
