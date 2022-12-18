61e890b49c54754f7616a3c158c365757f3b024d
i16fujimoto <fujimoto.ryusei@arakawa-lab.com>
2022-12-18 16:25:47 +0900

    回答追加のAPIを作成 & バグ修正

--- go-api/entity/request_entity/body/question.go
+++ go-api/entity/request_entity/body/question.go
@@ -9,8 +9,13 @@ type PostQuestionBody struct {
 	CommunityId string `json:"community binding:"required"`
 	Title string `json:"title" binding:"required"`
 	Detail string `json:"detail" binding:"required"`
-	Image [][]byte `json:"image" binding:"required"`
+	Image [][]byte `json:"image"`
 	Priority string `json:"priority" binding:"required"`
 	Status string `json:"status" binding:"required"`
 	Category []string `json:"category" binding:"required"`
+}
+
+type PostAnswerBody struct {
+	Detail string `json:"detail" binding:"required"`
+	Images [][]byte `json:"images"`
 }
